cd92c52347e3e05cb430e732c005db48d9ebb2e0
Marwa Hussein <125766604+marwahussein04@users.noreply.github.com>
2025-09-06 21:24:45 +0300

    Add files via upload

---
+++ views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Customer
+
+def customer_list(request):
+    customers = Customer.objects.all()
+    return render(request, 'customers/customer_list.html', {'customers': customers})
+
+def customer_detail(request, pk):
+    customer = get_object_or_404(Customer, pk=pk)
+    return render(request, 'customers/customer_detail.html', {'customer': customer})
+
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Customer
+from .forms import CustomerForm
+
+def customer_create(request):
+    if request.method == 'POST':
+        form = CustomerForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('customers:customer_list')
+    else:
+        form = CustomerForm()
+    return render(request, 'customers/customer_form.html', {'form': form})
+
+
+def customer_update(request, pk):
+    customer = get_object_or_404(Customer, pk=pk)
+    if request.method == 'POST':
+        form = CustomerForm(request.POST, instance=customer)
+        if form.is_valid():
+            form.save()
+            return redirect('customers:customer_list')
+    else:
+        form = CustomerForm(instance=customer)
+    return render(request, 'customers/customer_form.html', {'form': form})
+
+def customer_delete(request, pk):
+    customer = get_object_or_404(Customer, pk=pk)
+    if request.method == 'POST':
+        customer.delete()
+        return redirect('customer_list')
+    return render(request, 'customers/customer_confirm_delete.html', {'customer': customer})
